5b3e8b19b4839e25d8309b9d908c76115a33adee
Nirmal Krishnan <nkrishn9@jhu.edu>
2016-05-15 18:33:55 -0400

    cleaned up the view

--- pills.py
+++ pills.py
@@ -5,25 +5,26 @@ from twilio.rest import TwilioRestClient
 phoneNumber = -1
 
 def main():
+	os.system('clear')
 	choice = printMenu()
 	while choice != 6:
-		print(choice)
+		os.system('clear')
 		if choice == 1:
-			print("1 selected")
+			print("Set phone number")
 		elif choice == 2:
-			print("2 selected")
+			print("Add a medication")
 		elif choice == 3:
-			print("3 selected")
+			print("Delete a medication")
 		elif choice == 4:
-			print("4 selected")
+			print("View all medications")
 		else:
-			print("5 selected")
+			print("Start Listening")
 		choice = printMenu()
 
 
 def printMenu():
 	if phoneNumber == -1:
-		print("\nPhone Number: NOT SET")
+		print("Phone Number: NOT SET\n")
 	else:
 		print("Phone Number: " + phoneNumber)
 	print("1) Set phone number\n2) Add a medication\n3) Delete a medication\n4) View all medications\n5) Start Listening\n6) Quit")
