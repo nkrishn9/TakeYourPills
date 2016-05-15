a90c8c8f31f8de8e5a320527a16c6a4c46b506a1
Nirmal Krishnan <nkrishn9@jhu.edu>
2016-05-15 15:16:36 -0400

    added files

---
+++ TwilioFun.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from twilio.rest import TwilioRestClient
+
+def main():
+	#Enter your Twilio account information below:
+	ACCOUNT_SID = os.environ["ACCOUNT_SID"]
+	AUTH_TOKEN = os.environ["API_KEY"]
+
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+	client.messages.create(
+		to = os.environ["TO"],
+		from_ = os.environ["FROM"],
+		body = "Take your multivitamins now",
+	)
+
+if __name__ == "__main__":
+	main()
